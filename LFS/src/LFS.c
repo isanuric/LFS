817c666d9a0d3346385d7d0b86e96664d74c2239
isanuric <isanuric@gmail.com>
2016-05-10 18:03:47 +0200

    struct2

--- LFS/src/LFS.c
+++ LFS/src/LFS.c
@@ -5,26 +5,39 @@
  *      Author: ehsan
  */
 #include "header.h"
-double X1,  Y1,  Z1,  X2,  Y2,  Z2, Xn,  Yn,  Zn;
-double L_str;
+double Xn,  Yn,  Zn;
 
 
 
 //void do_CIE(double X1, double Y1, double Z1, double Xn, double Yn, double Zn)
 void do_CIE()
 {
+	double X1, Y1, Z1;
+	double *inputs, *inputs2;
+	struct_Stars param_star1, param_star2, param_star3, param_star4;
+	print_header();		   // print program header
+	inputs = get_param();  // get user parameters
+	inputs2 = inputs + 3;  // get user parameters
 
-	print_header();		// print program header
-	get_param();		// get user parameters
-	calc_CIE();			// CIE Normalvalenzsystem
-	calc_CIE_Lab();		// CIE-L*a*b* - Farbraumsystem
-	calc_CIE_Luv();		// CIE-L*v*u* System
+	Xn = inputs[MAX-3];
+	Yn = inputs[MAX-2];
+	Zn = inputs[MAX-1];
+
+	calc_CIE(inputs[0], inputs[1], inputs[2]);		// CIE Normalvalenzsystem
+	param_star1 = calc_CIE_Lab(inputs);				// CIE-L*a*b* - Farbraumsystem
+	param_star2 = calc_CIE_Lab(inputs2);			// CIE-L*a*b* - Farbraumsystem
+	calc_delta_e(param_star1.a, param_star2.a);		// delta E
+
+	printf("***********************************************************\n");
+	param_star3 = calc_CIE_Luv(inputs);			// CIE-L*v*u* System
+	param_star4 = calc_CIE_Luv(inputs2);		// CIE-L*v*u* System
+	calc_delta_e(param_star3.a, param_star4.a);	// delta E
  }
 
 /*
  * CIE Normalvalenzsystem
  */
-void calc_CIE()
+void calc_CIE(double X1, double Y1, double Z1)
 {
 	double x1, y1,z1;
 	double denominator;
@@ -32,7 +45,7 @@ void calc_CIE()
 	x1 = X1/denominator;
 	y1 = Y1/denominator;
 	z1 = Z1/denominator;
-	printf("CIE-Normalvalenzsystem:\n");
+	printf("CIE-Normalvalenzsystem for X=%.3lf, Y=%.3lf, Z=%.3lf\n", X1, Y1, Z1);
 	printf("x     = %.3f \n", x1);
 	printf("y     = %.3f \n", y1);
 	printf("z     = %.3f \n", z1);
@@ -43,64 +56,84 @@ void calc_CIE()
 /*
  * CIE-L*a*b* - Farbraumsystem
  */
-void calc_CIE_Lab()
+struct_Stars calc_CIE_Lab(double *param)
 {
-	double a_str, b_str, h_ab, C_str_ab;
+	double  h_ab, C_str_ab;
 	double X1_Xn, Y1_Yn, Z1_Zn, limit;
-	Y1_Yn = Y1/Yn;
-	X1_Xn = X1/Xn;
-	Z1_Zn = Z1/Zn;
+	struct_Stars struct_lab;
+	double lab[3];
+	X1_Xn = param[0]/Xn;
+	Y1_Yn = param[1]/Yn;
+	Z1_Zn = param[2]/Zn;
 	limit = pow( 6.0/29, 3);  // 0.008856
-	L_str = 116 * SQRN(3, Y1_Yn) - 16;
+	struct_lab.a[2] = 116 * SQRN(3, Y1_Yn) - 16;
 	// calculate a* and b*:
 	if(X1_Xn > limit || Y1_Yn > limit)	{
-		a_str = 500 * ( do_sqr3(X1_Xn) - do_sqr3(Y1_Yn) );
-		b_str = 200 * ( do_sqr3(Y1_Yn) - do_sqr3(Z1_Zn) );
+		struct_lab.a[0] = 500 * ( do_sqr3(X1_Xn) - do_sqr3(Y1_Yn) );
+		struct_lab.a[1] = 200 * ( do_sqr3(Y1_Yn) - do_sqr3(Z1_Zn) );
 	}else{
-		a_str = 200 * ( (841/108) * ( X1_Xn - Y1_Yn ) );
-		b_str = 200 * ( (841/108) * ( Y1_Yn - Z1_Zn ) );
+		struct_lab.a[0] = 200 * ( (841/108) * ( X1_Xn - Y1_Yn ) );
+		struct_lab.a[1] = 200 * ( (841/108) * ( Y1_Yn - Z1_Zn ) );
 	}
 	// calculate h_ab
-	h_ab = atan(b_str/a_str) * (180 / M_PI);  // radians into degree: (180 / M_PI)
-	if(a_str < 0)	{
-		if(b_str > 0)
+	h_ab = atan(struct_lab.a[1]/struct_lab.a[0]) * (180 / M_PI);  // radians into degree: (180 / M_PI)
+	if(lab[0] < 0)	{
+		if(struct_lab.a[1] > 0)
 			h_ab += 180;
 		else
 			h_ab -= 180;
 	}
 	// C*_ab
-	C_str_ab = sqrt( ( pow(a_str,2) + pow(b_str, 2) ) );
-	printf("CIE-L*a*b* Farbraumsystem:\n");
-	printf("L*    = %.3f \n", L_str);
-	printf("a*    = %.3f \n", a_str);
-	printf("b*    = %.3f  \n", b_str);
+	C_str_ab = sqrt( ( pow(lab[0],2) + pow(lab[1], 2) ) );
+	printf("CIE-L*a*b* Farbraumsystem for X=%.3lf, Y=%.3lf, Z=%.3lf\n", param[0], param[1], param[2]);
+	printf("L*    = %.3f \n", struct_lab.a[2]);
+	printf("a*    = %.3f \n", struct_lab.a[0]);
+	printf("b*    = %.3f  \n", struct_lab.a[1]);
 	printf("h_ab  = %.3f \n", h_ab);
 	printf("C*_ab = %.3f \n\n", C_str_ab);
+	return struct_lab;
 }
 
-void calc_CIE_Luv()
+/*
+ * CIE Luv
+ */
+struct_Stars calc_CIE_Luv(double *param)
 {
-	double u_str, v_str, delta_E;
-	mystruct uv   = calc_uv(X1, Y1, Z1);
-	mystruct uv_n = calc_uv(Xn, Yn, Zn);
-	u_str = 13.0 * L_str * (uv.u - uv_n.u);
- 	v_str = 13.0 * L_str * (uv.v - uv_n.v);
- 	delta_E =
-	printf("CIE-L*v*u* Farbraumsystem:\n");
-	printf("L* = %.3f \n", L_str);
-	printf("u  = %.3f \n", uv.u);
-	printf("v  = %.3f \n", uv.v);
-	printf("u_n  = %.3f \n", uv_n.u);
-	printf("v_n  = %.3f \n", uv_n.v);
-	printf("u* = %.3f \n", u_str);
-	printf("v* = %.3f \n", v_str);
+	struct_Stars struct_stars;
+	struct_Luv luv    = calc_uv(param[0], param[1], param[2]);
+	struct_Luv luv_n  = calc_uv(Xn,  Yn,  Zn);
+	struct_stars.a[0] = 13.0 * luv.l * (luv.u - luv_n.u);
+	struct_stars.a[1] = 13.0 * luv.l * (luv.v - luv_n.v);
+	struct_stars.a[2] = luv.l;
+	printf("CIE-L*v*u* Farbraumsystem for X=%.3lf, Y=%.3lf, Z=%.3lf\n", param[0], param[1], param[2]);
+	printf("L* = %.3f \n", luv.l);
+	printf("u  = %.3f \n", luv.u);
+	printf("v  = %.3f \n", luv.v);
+	printf("u_n  = %.3f \n", luv_n.u);
+	printf("v_n  = %.3f \n", luv_n.v);
+	printf("u* = %.3f \n", struct_stars.a[0]);
+	printf("v* = %.3f \n\n", struct_stars.a[1]);
+	return struct_stars;
 }
 
-mystruct calc_uv(double a, double b, double c)
+/*
+ * delta E
+ */
+void calc_delta_e(double *param, double *param2)
+{	double delta_e;
+	delta_e =  sqrt( pow((param[2]-param2[2]), 2) + pow((param[1]-param2[1]), 2) + pow((param[0]-param2[0]), 2) );
+	printf("ΔE = %.3f \n\n", delta_e);
+}
+
+/*
+ * calculate u and v
+ */
+struct_Luv calc_uv(double a, double b, double c)
 {
-	mystruct retval;
+	struct_Luv retval;
 	double denominator;
 	denominator = a + 15*b + 3*c;
+	retval.l = 116 * SQRN(3, b/Yn) - 16;
 	retval.u = 4*a / denominator;
 	retval.v = 6*b / denominator;
 	retval.v *= 1.5;
@@ -118,23 +151,19 @@ double do_sqr3(double a)
 /*
  * get the user parameters
  */
-double get_param()
+double *get_param()
 {
 	int var;
 	// get values
-	static double input_array[6];
+	static double input_array[MAX];
 	printf("Please enter X Y Z Xn Yn Zn: ");
 
-	for (var = 0; var < 6; ++var) {
+	for (var = 0; var < MAX; ++var) {
 		scanf("%lf", &input_array[var]);
 	}
 	printf("\n");
-	X1 = input_array[0];
-	Y1 = input_array[1];
-	Z1 = input_array[2];
-	Xn = input_array[3];
-	Yn = input_array[4];
-	Zn = input_array[5];
+	return input_array;
+
 }
 
 /*
--- LFS/src/header.h
+++ LFS/src/header.h
@@ -13,23 +13,28 @@
 
 #define  dim 3 // matrix dimension
 #define SQRN(m, x) pow(x, 1.0/m)
+#define MAX 9
 
 
 //
-typedef struct{ double u,v; }mystruct;
+typedef struct{ double l, u, v, u_str, v_str; }struct_Luv;
+typedef struct{ double a[3]; }struct_Stars;
 
 
 /*
  *
  */
 void   do_CIE();
-void   calc_CIE();
-void   calc_CIE_Lab();
+void   calc_CIE(double a, double b, double c);
+void   calc_delta_e(double *param, double *param2);
 void   print_header();
 double do_sqr3(double a);
-double get_param();
-//double *calc_uv(double X, double Y, double Z);
-mystruct calc_uv(double a, double b, double c);
+double *get_param();
+struct_Stars calc_CIE_Lab(double *param);
+struct_Stars calc_CIE_Luv(double *param);
+struct_Luv   calc_uv(double a, double b, double c);
+
+
 
 
 #endif /* MYHEADER_H */
